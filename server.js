4712acee1cfc665736278549569eed24cd4110e9
salman sajid <salman.sajid@hotmail.com>
2016-08-24 03:51:48 +0500

    ponka

--- server.js
+++ server.js
@@ -26,5 +26,6 @@ require('./app/routes')(app); // pass our application into our routes
 
 // start app ===============================================
 app.listen(port);	
+
 console.log('Magic happens on port ' + port); 			// shoutout to the user
 exports = module.exports = app; 						// expose app
